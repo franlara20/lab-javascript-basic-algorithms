2a923f496c586e8ac598e0c73f26311c0a56d8f2
Fran Lara <francesc.lara@hotmail.com>
2021-05-31 16:30:49 +0200

    iteration 1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 // Iteration 1: Names and Input
-console.log("hola");
+let hacker1 = "Fran";
+let hacker2 = "Irene";
+console.log(`The driver's name is, ${hacker1}`);
+console.log(`The navigator's name is, ${hacker2}`);
 
 // Iteration 2: Conditionals
 
